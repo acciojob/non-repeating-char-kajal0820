6ca2acbe030e70639354fc85870d53db50227c16
kajal0820 <130426994+kajal0820@users.noreply.github.com>
2025-05-12 11:08:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	for(let i=0;i<=str.length;i++)
+		let check =true;
+	for(let j=0;j<str.length;j++)
+		{
+			if(i!=j && str[i]==str[j])
+			{
+				check =false;
+				break;
+			}
+		}
+	if(check==true)
+	{
+		return str[i]
+	}
+	
+	}
+return null;
+	
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+//const input = prompt("Enter a string");
+//alert(firstNonRepeatedChar(input)); 
